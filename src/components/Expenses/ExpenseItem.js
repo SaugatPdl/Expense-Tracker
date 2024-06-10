266f2be76060a441d8fe82e51158abe05c4789c7
SaugatPdl <saugat1234567890@gmail.com>
2024-06-10 11:54:15 +0545

    Updated with the insertion of our data

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+ 
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      {/* Hamile yeta mathi expense date use gareko */}
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs. {props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
