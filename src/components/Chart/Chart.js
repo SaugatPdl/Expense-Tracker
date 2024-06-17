d55bc4eb93da9d715803b4a35156db43130acb9c
SaugatPdl <saugat1234567890@gmail.com>
2024-06-17 18:36:00 +0545

    Charts milara gareko samma

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,15 +1,17 @@
 import React from "react";
+
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
-    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
-    const totalMaximum = Math.max(...dataPointValues); // spread operator halera array gareko
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
-    <div className="Chart">
+    <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
-        key = {dataPoint.label}
+          key={dataPoint.label}
           value={dataPoint.value}
           maxValue={totalMaximum}
           label={dataPoint.label}
@@ -18,5 +20,4 @@ const Chart = (props) => {
     </div>
   );
 };
-
 export default Chart;
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,23 +1,23 @@
-import React from "react";
-import "./ChartBar.css";
+import React from 'react';
+
+import './ChartBar.css';
 
 const ChartBar = (props) => {
-    let barFillHeight = "0%";
-    if (props.maxValue > 0)  {
-        barFillHeight = Math.round((props.value / props.maxValue)*100) + "%";
-    }
+  let barFillHeight = '0%';
 
-
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+  }
 
   return (
-    <div className="ChartBar">
-      <div className="ChartBar__inner">
+    <div className='chart-bar'>
+      <div className='chart-bar__inner'>
         <div
-          className="ChartBar__fill"
-          style={{ height: barFillHeight}}
+          className='chart-bar__fill'
+          style={{ height: barFillHeight }}
         ></div>
       </div>
-      <div className="ChartBar__label">{props.label}</div>
+      <div className='chart-bar__label'>{props.label}</div>
     </div>
   );
 };
